069fe9ff24a0351b004af79ce96f58bd51262a71
gabeshaughnessy <gabeshaughnessy@gmail.com>
2019-08-08 14:42:18 -0700

    psuedo-code thoughts on how to work this

--- content-deploy.php
+++ content-deploy.php
@@ -120,6 +120,22 @@ function wpcd_batch_page() {
 		}
 		else{
 			// Create Batch
+			 //for each post type, including media
+				 //generate lists of remote site and local site content
+				 //compare time stamps
+				 //add local items not found on the remote site to the list of modified content
+				 //add local items with more recent time stamps to the list of modified content
+				 //output the list items as checkbox fields organized by post type
+				 		//include post title, local modification date, remote modification date (or new)
+			//Preview
+				//Display only selected content to be deployed in the batch with warning message.
+
+			//Send batch
+				//after selecting items to deploy and previewing deployment
+				//series of posts to remote site rest endpoint
+				//Remote site responds with success or error for each post
+
+				//what about uploads? Do sites get set up with a shared uploads directory?
 		}
 
 	echo '</div>';
